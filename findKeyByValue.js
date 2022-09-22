f780c79f0de4e23fe7dea0a54286261ef3f7c00c
Margaryta <margaryta.lanova@gmail.com>
2022-09-21 20:14:08 -0700

    search for a key on an object where its value mathes a given value

---
+++ findKeyByValue.js
@@ -0,0 +1,31 @@
+const assertEqual = function(actual , expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected} `)
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`)
+  }
+  
+  };
+  // Test code
+  
+  console.assert(assertEqual(1,1));
+  
+  const findKeyByValue = function(object , value) {
+    let result;
+    let genres = Object.keys(object)
+    for (let element of genres) {
+      if (value === object[element]) {
+       return element;
+      }
+    }
+    return undefined
+    }
+   
+  const bestTVShowsByGenre = { 
+    sci_fi: "The Expanse",
+    comedy: "Brooklyn Nine-Nine",
+    drama:  "The Wire"
+  };
+  
+  assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+  assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
