9c3f7d709a2042f15ab19433384c8a1372613236
Margaryta <margaryta.lanova@gmail.com>
2022-09-22 18:55:52 -0700

    our version of map

---
+++ map.js
@@ -0,0 +1,47 @@
+
+const assertEqual = function(actual , expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected} `);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+  
+};
+ 
+const eqArrays = function(firstArray,secondArray) {
+  if (firstArray.length !== secondArray.length) {
+    return false;
+  }
+  for (let i = 0; i < firstArray.length; i++) {
+    if (firstArray[i] !== secondArray[i]) {
+      return false;
+    }
+       
+  }
+  return true;
+};
+// eqArrays([1, 2, 3], [1, 2, 3]); // => true
+// eqArrays([1, 2, 3], [3, 2, 1]); // => false
+
+// eqArrays(["1", "2", "3"], ["1", "2", "3"]); // => true
+// eqArrays(["1", "2", "3"], ["1", "2", 3]); // => false
+//assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
+
+const assertArraysEqual = function(firstArray, secondArray) {
+  if (eqArrays(firstArray,secondArray)) {
+    console.log(`✅✅✅ Assertion Passed: ${firstArray} === ${secondArray}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${firstArray} !== ${secondArray}`);
+  }
+};
+const map = function(array, callback) {
+  const results = [];
+  for (let item of array) {
+    results.push(callback(item));
+  }
+  return results;
+}
+const words = ["ground", "control", "to", "major", "tom"];
+const results1 = map(words, word => word[0]);
+//console.log(results1);
+assertArraysEqual(eqArrays(results1, [ 'g', 'c', 't', 'm', 't' ]),true); // => true
