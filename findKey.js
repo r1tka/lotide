a73f45fd5164afb91536a64c020071e2912a6593
Margaryta <margaryta.lanova@gmail.com>
2022-09-24 14:49:51 -0700

    function which takes an object and callback and returns the first key for which the callback returns truthy

---
+++ findKey.js
@@ -0,0 +1,30 @@
+const assertEqual = function(actual , expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected} `);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+  
+};
+  // function which takes in an object and a callback
+  // should scan the object and return the first key for which the callback returns a truthy value.
+  // If no key is found, then it should return undefined.
+const findKey = function(object,callback) {
+  let result;
+  for (const key in object) {
+    if (callback(object[key])) {
+      result = key;
+      break;
+    }
+  }
+  return result;
+};
+
+console.log(findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+}, x => x.stars === 2)); // => "noma"
