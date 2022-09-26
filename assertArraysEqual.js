90ebcfa2847f6912d6d6178296270720375ace2b
Margaryta <margaryta.lanova@gmail.com>
2022-09-26 14:18:27 -0700

    rafactoring

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,33 +1,3 @@
-const assertEqual = function(actual , expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected} `);
-  } else {
-    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
-  }
-  
-};
-  // Test code
-  
-console.assert(assertEqual(1,1));
-
-const eqArrays = function(firstArray,secondArray) {
-  if (firstArray.length !== secondArray.length) {
-    return false;
-  }
-  for (let i = 0; i < firstArray.length; i++) {
-    if (firstArray[i] !== secondArray[i]) {
-      return false;
-    }
-       
-  }
-  return true;
-};
-eqArrays([1, 2, 3], [1, 2, 3]); // => true
-eqArrays([1, 2, 3], [3, 2, 1]); // => false
-
-eqArrays(["1", "2", "3"], ["1", "2", "3"]); // => true
-eqArrays(["1", "2", "3"], ["1", "2", 3]); // => false
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
 
 const assertArraysEqual = function(firstArray, secondArray) {
   if (eqArrays(firstArray,secondArray)) {
@@ -36,3 +6,6 @@ const assertArraysEqual = function(firstArray, secondArray) {
     console.log(`🛑🛑🛑 Assertion Failed: ${firstArray} !== ${secondArray}`);
   }
 };
+
+
+module.exports = assertArraysEqual
