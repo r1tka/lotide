c19b1573dba3405a223215b8db439e2049e688d9
Margaryta <margaryta.lanova@gmail.com>
2022-09-26 14:09:46 -0700

    refactoring

--- eqArrays.js
+++ eqArrays.js
@@ -1,15 +1,3 @@
-const assertEqual = function(actual , expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected} `);
-  } else {
-    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
-  }
-  
-};
-  // Test code
-  
-console.assert(assertEqual(1,1));
-
 const eqArrays = function(firstArray,secondArray) {
   if (firstArray.length !== secondArray.length) {
     return false;
@@ -22,9 +10,5 @@ const eqArrays = function(firstArray,secondArray) {
   }
   return true;
 };
-eqArrays([1, 2, 3], [1, 2, 3]); // => true
-eqArrays([1, 2, 3], [3, 2, 1]); // => false
 
-eqArrays(["1", "2", "3"], ["1", "2", "3"]); // => true
-eqArrays(["1", "2", "3"], ["1", "2", 3]); // => false
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
+module.exports = eqArrays
