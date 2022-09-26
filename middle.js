5335d0e256f6e89ba1c7c7a494a0739bbbb88463
Margaryta <margaryta.lanova@gmail.com>
2022-09-26 14:23:42 -0700

    rafactoring

--- middle.js
+++ middle.js
@@ -1,29 +1,4 @@
 
-const eqArrays = function(firstArray,secondArray) {
-  if (firstArray.length !== secondArray.length) {
-    return false;
-  }
-  for (let i = 0; i < firstArray.length; i++) {
-    if (firstArray[i] !== secondArray[i]) {
-      return false;
-    }
-       
-  }
-  return true;
-};
-eqArrays([1, 2, 3], [1, 2, 3]); // => true
-eqArrays([1, 2, 3], [3, 2, 1]); // => false
-
-eqArrays(["1", "2", "3"], ["1", "2", "3"]); // => true
-eqArrays(["1", "2", "3"], ["1", "2", 3]); // => false
-
-const assertArraysEqual = function(firstArray, secondArray) {
-  if (eqArrays(firstArray,secondArray)) {
-    console.log(`✅✅✅ Assertion Passed: ${firstArray} === ${secondArray}`);
-  } else {
-    console.log(`🛑🛑🛑 Assertion Failed: ${firstArray} !== ${secondArray}`);
-  }
-};
 // Function should return an array with only the middle element(s) of the provided array;
 // For arrays with one or two elements return an empty array
 // For arrays with odd number of element, single middle element should return
@@ -42,10 +17,4 @@ const assertArraysEqual = function(firstArray, secondArray) {
     return [array[oddIndex]]
    }
  }
-   // test run
-   console.log(middle([1])) // => []
-   console.log(middle([1, 2])) // => []
-   console.log(middle([1, 2, 3])) // => [2]
-   console.log(middle([1, 2, 3, 4, 5])) // => [3]
-   console.log(middle([1, 2, 3, 4])) // => [2, 3]
-   console.log(middle([1, 2, 3, 4, 5, 6])) // => [3, 4]
+    module.exports = middle
