5a76c0b894db50789f0f6db4759feb49155392d9
Margaryta <margaryta.lanova@gmail.com>
2022-09-26 16:19:45 -0700

    refactoring with mocha and chai

--- test/tailTest.js
+++ test/tailTest.js
@@ -1,13 +1,33 @@
-const tail = require('../tail')
-const assertEqual = require('../assertEqual')
+const assert = require('chai').assert;
+const tail = require('../tail.js');
 
 
+describe("#tail", () => {
+  it("returns [2, 3] for [1, 2, 3]", () => {
+    assert.deepEqual(tail([1, 2, 3]), [2, 3]);
+  });
+  it("returns [] for ['5']", () => {
+    assert.deepEqual(tail(['5']), []); 
+  });
 
-const result = tail(["Hello", "Lighthouse", "Labs"]);
-assertEqual(result, ["Lighthouse", "Labs"]);
-//Test Case: Check the original array````
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words); // no need to capture the return value since we are not checking it
-assertEqual(words.length, 3); // original array should still have 3 elements!
+  it(`returns back two elements from ["Hello", "Lighthouse", "Labs"]`, () => {
+    assert.strictEqual(tail(["Hello", "Lighthouse", "Labs"]).length, 2); 
+  });
+
+  it(`returns back first element "Lighthouse" from ["Hello", "Lighthouse", "Labs"]`, () => {
+    assert.strictEqual(tail(["Hello", "Lighthouse", "Labs"])[0], "Lighthouse"); 
+  });
+
+  it(`returns back second element "Labs" from ["Hello", "Lighthouse", "Labs"]`, () => {
+    assert.strictEqual(tail(["Hello", "Lighthouse", "Labs"])[1], "Labs"); 
+  });
+});
+
+// const result = tail(["Hello", "Lighthouse", "Labs"]);
+// assertEqual(result, ["Lighthouse", "Labs"]);
+// //Test Case: Check the original array````
+// const words = ["Yo Yo", "Lighthouse", "Labs"];
+// tail(words); // no need to capture the return value since we are not checking it
+// assertEqual(words.length, 3); // original array should still have 3 elements!
 
 
